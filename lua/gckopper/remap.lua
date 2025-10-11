42a09c9feab2eee3e657b89e2dbbd6afd970fc11
Gabriel Kopper <41166074+gckopper@users.noreply.github.com>
2025-10-11 16:06:19 -0300

    new remaps for splits and tabs to replace tmux

--- lua/gckopper/remap.lua
+++ lua/gckopper/remap.lua
@@ -23,3 +23,37 @@ vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references)
 vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename)
 vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help)
+
+-- horizontal split with "
+vim.keymap.set("n", "<leader>\"", ":new<CR>")
+-- vertical split with %
+vim.keymap.set("n", "<leader>%", ":vnew<CR>")
+-- new tab with c (tmux like)
+vim.keymap.set("n", "<leader>c", ":tabnew<CR>")
+-- navigate between splits with alt hjkl
+vim.keymap.set("n", "<A-j>", "<C-w><Down>")
+vim.keymap.set("n", "<A-k>", "<C-w><Up>")
+vim.keymap.set("n", "<A-h>", "<C-w><Left>")
+vim.keymap.set("n", "<A-l>", "<C-w><Right>")
+-- go to terminal mode with t
+vim.keymap.set("n", "<leader>t", ":terminal<CR>")
+
+-- set current tab with alt n
+vim.keymap.set("n", "<A-1>", ":tabn 1<CR>")
+vim.keymap.set("n", "<A-2>", ":tabn 2<CR>")
+vim.keymap.set("n", "<A-3>", ":tabn 3<CR>")
+vim.keymap.set("n", "<A-4>", ":tabn 4<CR>")
+vim.keymap.set("n", "<A-5>", ":tabn 5<CR>")
+vim.keymap.set("n", "<A-6>", ":tabn 6<CR>")
+vim.keymap.set("n", "<A-7>", ":tabn 7<CR>")
+vim.keymap.set("n", "<A-8>", ":tabn 8<CR>")
+vim.keymap.set("n", "<A-9>", ":tabn 9<CR>")
+
+-- leave terminal mode with jk too
+vim.keymap.set("t", "jk", "<C-\\><C-n>")
+
+-- Move lines up and down
+vim.keymap.set("n", "<A-Up>", ":m-2<CR>")
+vim.keymap.set("n", "<A-Down>", ":m+<CR>")
+vim.keymap.set("v", "<A-Up>", ":m-2<CR>gv=gv")
+vim.keymap.set("v", "<A-Down>", ":m'>+<CR>gv=gv")
