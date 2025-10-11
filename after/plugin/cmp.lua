565844682253b98dfaa678d382df6a386d806091
Gabriel Kopper <41166074+gckopper@users.noreply.github.com>
2025-10-11 16:05:34 -0300

    use ctrl up down to scroll docs

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -32,8 +32,8 @@ cmp.setup({
         ['<Down>'] = cmp.mapping.select_next_item(select_opts),
         ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
 
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Up>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-Down>'] = cmp.mapping.scroll_docs(4),
 
         ['<C-e>'] = cmp.mapping.abort(),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
